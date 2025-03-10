9176f3dccd71bd7feb7a1998e1d06086b0e67145
MbusiHlatsh <mbusihlatsh@gmail.com>
2025-03-09 22:11:23 -0700

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Mbusi Hlatshwayo on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
